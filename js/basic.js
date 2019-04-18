c298d596940e572d1b4e49bc57b9a7689add3ec3
Noonmoon <tristandeering@hotmail.com>
2019-04-18 15:51:30 -0400

    init

---
+++ js/basic.js
@@ -0,0 +1,30 @@
+/*
+ * Basic sample
+*/
+
+$('.flipbook').turn({
+	// Width
+
+	width:922,
+	
+	// Height
+
+	height:600,
+
+	// Elevation
+
+	elevation: 50,
+	
+	// Enable gradients
+
+	gradients: true,
+	
+	// Auto center this flipbook
+
+	autoCenter: true
+
+});
+
+
+
+
